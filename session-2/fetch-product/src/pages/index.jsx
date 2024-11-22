5d5e0d4343b41e96976a937e4b7665b0a9a87ec3
Arian <arian.koochak@protonmail.com>
2024-11-22 20:06:40 +0330

    add get serverSideProps in main route

--- session-2/fetch-product/src/pages/index.jsx
+++ session-2/fetch-product/src/pages/index.jsx
@@ -1,6 +1,18 @@
+export default function index({data}) {
+  console.log(data);
+    return <div>index</div>;
+}
 
-export default function index() {
-  return (
-    <div>index</div>
-  )
+export async function getServerSideProps(data) {
+  try {
+    const response = await fetch("https://rickandmortyapi.com/api/character");
+    const data = await response.json();
+    return {
+      props:{
+        data
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
