b03f0dbb4ff253cf511036867e3cee0c07ac2f72
Arian <arian.koochak@protonmail.com>
2024-11-22 22:40:53 +0330

    add users template

--- session-2/fetch-product/src/pages/index.jsx
+++ session-2/fetch-product/src/pages/index.jsx
@@ -1,18 +1,34 @@
-export default function index({data}) {
-  console.log(data);
-    return <div>index</div>;
+export default function index({ characters }) {
+    return (
+        <div className="main-container">
+            {characters.map((character) => {
+                return (
+                    <div className="user-container" key={character.name}>
+                        <div className="user-image">
+                            <img src={character.image} alt={character.name} />
+                        </div>
+                        <div className="user-name">
+                            <h3>{character.name}</h3>
+                        </div>
+                    </div>
+                );
+            })}
+        </div>
+    );
 }
 
-export async function getServerSideProps(data) {
-  try {
-    const response = await fetch("https://rickandmortyapi.com/api/character");
-    const data = await response.json();
-    return {
-      props:{
-        data
-      }
+export async function getServerSideProps() {
+    try {
+        const response = await fetch(
+            "https://rickandmortyapi.com/api/character"
+        );
+        const data = await response.json();
+        return {
+            props: {
+                characters: data.results,
+            },
+        };
+    } catch (error) {
+        console.log(error);
     }
-  } catch (error) {
-    console.log(error);
-  }
 }
