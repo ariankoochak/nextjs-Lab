569313053d16876413aac6ed025e05b15823f345
Arian <arian.koochak@protonmail.com>
2024-11-24 11:53:03 +0330

    add log api

--- session-3/monitoring-system/src/pages/api/addLog.js
+++ session-3/monitoring-system/src/pages/api/addLog.js
@@ -1,5 +1,10 @@
+import path from 'path'
+import fs from 'fs'
+
 const Handler = (req,res)=>{
-    console.log(req.body);
+    const rowData = `${req.body.route} , ${Date.now()} \n`;
+    const dir = path.resolve("public/log.csv");
+    fs.appendFileSync(dir,rowData);
     res.send('ok!')
 }
 
