58ee7c6f50318eea229dcb3ac21f0f8f0284bca2
Arian <arian.koochak@protonmail.com>
2024-11-29 16:50:22 +0330

    add users page

--- sessoin-4/first-app-router/src/app/users/page.jsx
+++ sessoin-4/first-app-router/src/app/users/page.jsx
@@ -1,5 +1,25 @@
-export default function page() {
-  return (
-    <div>page</div>
-  )
+import Image from "next/image";
+
+export default async function page() {
+    const response = await fetch("https://rickandmortyapi.com/api/character");
+    const users = await response.json();
+    console.log(users.results);
+
+    return (
+        <>
+            {users.results.map((user) => {
+                return (
+                    <div
+                    key={user.id}
+                        className="user-container"
+                    >
+                        <div className="user-image-container">
+                            <Image src={user.image} alt={user.name} fill />
+                        </div>
+                        <h4>{user.name}</h4>
+                    </div>
+                );
+            })}
+        </>
+    );
 }
