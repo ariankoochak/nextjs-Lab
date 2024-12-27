0fd0bbb9b677d60a00d1e7a170de7ae520de8d1d
Arian <arian.koochak@protonmail.com>
2024-12-27 18:28:42 +0330

    add refresh token

--- Session-7/Auth/app/api/auth/route.js
+++ Session-7/Auth/app/api/auth/route.js
@@ -1,4 +1,4 @@
-import { SignJWT } from "jose";
+import { jwtVerify, SignJWT } from "jose";
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
@@ -8,14 +8,24 @@ export async function POST(request) {
   const cookieStore = await cookies();
   const expires = new Date(Date.now() + 60 * 1000);
   cookieStore.set('accessToken', token, {httpOnly: true, expires});
+
+  const expireRefreshToken = new Date(Date.now() + 60 * 60 * 1000);
+   const refreshToken = await JWTrefreshToken(payload);
+  cookieStore.set("expireRefreshToken", refreshToken, {
+      httpOnly: true,
+      expires: expireRefreshToken,
+  });
   return NextResponse.json({ message: "success" });
 }
+
 
 
 export async function DELETE(req){
   const cookieStore = await cookies();
 
   cookieStore.delete('accessToken');
+
+  cookieStore.delete("expireRefreshToken");
 
   return NextResponse.redirect(new URL("/login", req.url));
 }
@@ -34,4 +44,21 @@ const JWTgenerate = async (payload) => {
     .sign(secretKey);
 
   return token;
+};
+
+
+const JWTrefreshToken = async (payload) => {
+    const header = {
+        alg: "HS256",
+        typ: "JWT",
+    };
+
+    const secretKey = new TextEncoder().encode("harchi");
+    const token = await new SignJWT(payload)
+        .setProtectedHeader(header)
+        .setIssuedAt()
+        .setExpirationTime("20 minutes")
+        .sign(secretKey);
+
+    return token;
 };
--- Session-7/Auth/middleware.js
+++ Session-7/Auth/middleware.js
@@ -1,14 +1,57 @@
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
+import { jwtVerify, SignJWT } from "jose";
+
 
 export async function middleware(request) {
   if (request.nextUrl.pathname.includes("dashboard")) {
     const cookieStore = await cookies();
     const accessToken = cookieStore.get("accessToken");
     if (!accessToken) {
-      return NextResponse.redirect(new URL("/login", request.url));
+      const refreshToken = cookieStore.get("expireRefreshToken");
+      
+      if(!refreshToken){
+        return NextResponse.redirect(new URL("/login", request.url));
+      }
+      else{
+          const refreshToken = cookieStore.get("expireRefreshToken");
+          console.log(refreshToken);
+          const tmp = await JWTgetData(refreshToken.value);
+
+          const payload = {userName : tmp.userName,password : tmp.password};
+          const token = await JWTgenerate(payload);
+          const expires = new Date(Date.now() + 60 * 1000);
+          cookieStore.set('accessToken', token, {httpOnly: true, expires});
+          return NextResponse.next();
+      }
     } else {
       return NextResponse.next();
     }
   }
 }
+
+
+const JWTgetData = async (input) => {
+    const secretKey = new TextEncoder().encode("harchi");
+    const token = await jwtVerify(input, secretKey, {
+        algorithms: ["HS256"],      
+    });
+    return token;
+};
+
+
+const JWTgenerate = async (payload) => {
+  const header = {
+    alg: "HS256",
+    typ: "JWT",
+  };
+
+  const secretKey = new TextEncoder().encode("harchi");
+  const token = await new SignJWT(payload)
+    .setProtectedHeader(header)
+    .setIssuedAt()
+    .setExpirationTime("2 minutes")
+    .sign(secretKey);
+
+  return token;
+};
